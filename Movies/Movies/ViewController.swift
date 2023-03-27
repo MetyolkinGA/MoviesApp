9f54d6ccef7e2495595e445306d79126095cbfa5
MetyolkinGA <115951758+MetyolkinGA@users.noreply.github.com>
2023-03-27 22:22:23 +0500

    main

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © Movie. All rights reserved.
+
+import UIKit
+
+final class ViewController: UIViewController {
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
